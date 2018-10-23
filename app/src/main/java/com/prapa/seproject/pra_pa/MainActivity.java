7fcff34a2b6c79ab04950cf8695eabff500c315d
phpond <59070182@it.kmitl.ac.th>
2018-10-23 22:00:05 +0700

    Edit fragment home

--- app/src/main/java/com/prapa/seproject/pra_pa/MainActivity.java
+++ app/src/main/java/com/prapa/seproject/pra_pa/MainActivity.java
@@ -3,11 +3,16 @@ package com.prapa.seproject.pra_pa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.prapa.seproject.pra_pa.Fragment.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_showbill);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new HomeFragment()).commit();
+        }
     }
 }
